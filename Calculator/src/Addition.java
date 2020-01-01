75651fa5817afbfeb3af2f98637b396c0ad785f5
Harish Kumar Mishra <Harish Kumar Mishra@LAPTOP-8BG9MOQS>
2020-01-01 16:20:18 +0530

    First Commit

---
+++ Calculator/src/Addition.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Addition")
+public class Addition extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		
+		int a= (int) req.getAttribute("n1");
+
+		int b=(int) req.getAttribute("n2");
+		
+		int c=a+b;
+		
+		PrintWriter pr= res.getWriter();
+		pr.print("the addition is "+c);
+
+	
+	}
+}
+
